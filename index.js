570b3ce04bdec95c94669132c8e57a53b0b8a921
feliipecardosoo <fglibino@gmail.com>
2023-11-28 20:33:45 -0300

    Conectando o Mongo com o meu DB

---
+++ index.js
@@ -0,0 +1,20 @@
+const exp = require('constants')
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+const conn = require('./db/conn')
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+
+// read body
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
