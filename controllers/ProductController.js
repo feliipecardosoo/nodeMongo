21876af722228aa24bc287f2cda08a11d831545e
feliipecardosoo <fglibino@gmail.com>
2023-12-04 19:51:58 -0300

    adicionando imagem e mostrando produtos

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,23 +1,27 @@
 const Product = require('../models/Product')
 
 module.exports = class ProductController {
-    static showProducts(req,res) {
-        res.render('products/all')
+    static async showProducts(req,res) {
+
+        const products = await Product.getProducts()
+
+        res.render('products/all', {products})
     }
     static createProduct(req,res) {
         res.render('products/create')
     }
     static async createProductPost (req,res) {
         const name = req.body.name
+        const image = req.body.image
         const price = req.body.price
         const description = req.body.description
 
-        const product = new Product (name, price, description)
+        const product = new Product (name, image, price, description)
 
         product.save()
 
         console.log(`O produto: ${name} foi criado preço de ${price} com a descriçao: ${description}`)
 
-        res.render('products/all')
+        res.redirect('/products')
     }
 }
